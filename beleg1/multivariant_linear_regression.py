fee3a8666e8aa691f55ca87a7c9edaebe5a73af4
Roechi <msg.remus@gmail.com>
2015-05-25 16:36:10 +0200

    Added plots and gradient decent.

--- beleg1/multivariant_linear_regression.py
+++ beleg1/multivariant_linear_regression.py
@@ -2,29 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-x_min = -10.
-x_max = 10.
-m = 10
-
-x1 = np.random.uniform(x_min, x_max, m)
-x2 = np.random.uniform(x_min, x_max, m)
-
-X = np.column_stack([x1, x2])
-
-theta = np.random.uniform(x_min, x_max, 2)
-
 def linear_hypothesis(theta):
     return lambda x: x.dot(theta)
-
-h = linear_hypothesis(theta)
-
-Y = h(X) + np.random.normal(-1, 1, m);
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.scatter(x1, x2, zs=Y, zdir=u'z', s=20, c=u'b', depthshade=True)
-fig.show()
 
 def cost_function(x, y):
     assert (len(x) == len(y))
@@ -37,3 +16,48 @@ def compute_new_theta(x, y, theta, alpha):
     m = len(x)
     return (theta - alpha * (1./float(m) * \
     x.T.dot(linear_hypothesis(theta)(x) - y)))
+
+def gradient_decent(alpha, theta, nb_iterations, X, y):
+    res = []
+    j = cost_function(X, y);
+    for x in range(0, nb_iterations):
+        res.append(j(theta))
+        theta = compute_new_theta(X, y, theta, alpha)
+    return res
+
+x_min = -10.
+x_max = 10.
+m = 10
+
+x1 = np.random.uniform(x_min, x_max, m)
+x2 = np.random.uniform(x_min, x_max, m)
+
+X = np.column_stack([x1, x2])
+
+theta = np.random.normal(-10, 10, 2)
+
+h = linear_hypothesis(theta)
+
+Y = h(X) + np.random.normal(-5, 5, m)
+
+plt.figure(1)
+#sct = fig.add_subplot(111, projection='3d')
+#plt.figure(1)
+plt.subplot(111, projection='3d')
+
+#sct.scatter(x1, x2, zs=Y, zdir=u'z', s=20, c=u'r', depthshade=True)
+plt.scatter(x1, x2, zs=Y, zdir=u'z', s=20, c=u'r', depthshade=True)
+
+it = 500
+#theta = np.array([-1, 3])
+alpha = 0.001
+res = gradient_decent(alpha, theta, it, X, Y)
+
+#grd = fig.add_subplot(111)
+#grd.plot(range(0, it), res)
+
+plt.figure(2)
+plt.plot(range(0, it), res)
+
+plt.show()
+
