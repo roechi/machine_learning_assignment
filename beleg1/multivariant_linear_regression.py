a0ad92708b605a5a6b433a7d76aa88b1aca793e3
Roechi <msg.remus@gmail.com>
2015-05-25 17:49:57 +0200

    Added plot for results.

--- beleg1/multivariant_linear_regression.py
+++ beleg1/multivariant_linear_regression.py
@@ -23,7 +23,7 @@ def gradient_decent(alpha, theta, nb_iterations, X, y):
     for x in range(0, nb_iterations):
         res.append(j(theta))
         theta = compute_new_theta(X, y, theta, alpha)
-    return res
+    return res, theta
 
 x_min = -10.
 x_max = 10.
@@ -41,23 +41,25 @@ h = linear_hypothesis(theta)
 Y = h(X) + np.random.normal(-5, 5, m)
 
 plt.figure(1)
-#sct = fig.add_subplot(111, projection='3d')
-#plt.figure(1)
 plt.subplot(111, projection='3d')
 
-#sct.scatter(x1, x2, zs=Y, zdir=u'z', s=20, c=u'r', depthshade=True)
 plt.scatter(x1, x2, zs=Y, zdir=u'z', s=20, c=u'r', depthshade=True)
 
 it = 500
 #theta = np.array([-1, 3])
 alpha = 0.001
-res = gradient_decent(alpha, theta, it, X, Y)
-
-#grd = fig.add_subplot(111)
-#grd.plot(range(0, it), res)
+res, theta = gradient_decent(alpha, theta, it, X, Y)
 
 plt.figure(2)
 plt.plot(range(0, it), res)
 
+fig = plt.figure(3)
+ax = fig.add_subplot(111, projection='3d')
+xp = yp = range(-10, 10, 1)
+XP, YP = np.meshgrid(xp, yp)
+zp = np.array(linear_hypothesis(theta)(np.column_stack([xp, yp])))
+
+ax.scatter(x1, x2, zs=linear_hypothesis(theta)(X), zdir=u'z', s=20, c=u'b', depthshade=True)
+ax.scatter(x1, x2, zs=Y, zdir=u'z', s=20, c=u'r', depthshade=True)
 plt.show()
 
